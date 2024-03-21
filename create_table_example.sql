6500d262c0d7c05d106604cd417223ec31c6b0ce
Geran123ss <germanmendieta123ss@gmail.com>
2024-03-21 18:48:36 -0300

    add pt2

---
+++ create_table_example.sql
@@ -0,0 +1,8 @@
+-- Script para crear la tabla "personas"
+CREATE TABLE personas (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    edad INTEGER NOT NULL,
+    country_code VARCHAR(10) NOT NULL
+);
